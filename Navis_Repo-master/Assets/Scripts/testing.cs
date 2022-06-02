3a7b2425713665498ac2ac1047ebc91866db4498
KeeperOfHonor <Christian@fam-oostenbrug.nl>
2022-06-02 07:56:00 +0100

    unity test scene

---
+++ Navis_Repo-master/Assets/Scripts/testing.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class testing : MonoBehaviour
+{
+    private void Start()
+    {
+        Grid grid = new Grid(20,10);
+    }
+}
