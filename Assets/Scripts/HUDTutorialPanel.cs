52dd7307528029c8c47d3f645294c5df53b00d0b
Justin Sijbolts <partydoos@gmail.com>
2022-06-17 10:16:26 +0200

    QoL
    Added countdown to Understood button

--- Assets/Scripts/HUDTutorialPanel.cs
+++ Assets/Scripts/HUDTutorialPanel.cs
@@ -7,10 +7,22 @@ public class HUDTutorialPanel : MonoBehaviour
 {
     private Button TutorialButtonClose;
     private GameObject TutorialPanel1;
+    public Text ConfirmBtnTxt;
     void Start()
     {
         TutorialButtonClose = GameObject.Find("ButtonCL").GetComponent<Button>();
         TutorialButtonClose.onClick.AddListener(ClosePanel1);
+
+        StartCoroutine(ButtonActivator());
+    }
+
+    void Update() {
+        Timer();
+        if (timeRemaining > 0) {
+            ConfirmBtnTxt.text = "Understood (" + Mathf.Ceil(timeRemaining) + ")";
+        } else {
+            ConfirmBtnTxt.text = "Understood";
+        }
     }
 
     void ClosePanel1() {
@@ -19,5 +31,22 @@ public class HUDTutorialPanel : MonoBehaviour
             TutorialPanel1.SetActive(false);
     }
 
+    private IEnumerator ButtonActivator() {
+    while(true) {
+        TutorialButtonClose.interactable = false;
+        yield return new WaitForSeconds(5);
+        TutorialButtonClose.interactable = true;
+        break;
+    }
+}
 
+    private float timeRemaining = 10;
+    void Timer() {
+        if (timeRemaining > 0 ) {
+            timeRemaining -= Time.deltaTime;
+            TutorialButtonClose.interactable = false;
+        } else {
+            TutorialButtonClose.interactable = true;
+        }
+    }
 }
